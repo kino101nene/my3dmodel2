4e40c678750381accd5a6d1b14b276b4643b165b
kino101nene <148795000+kino101nene@users.noreply.github.com>
2023-11-09 14:11:25 +0100

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,3 +1,5 @@
+
+console.log("Hello, World!");
 $w.onReady(function () {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
